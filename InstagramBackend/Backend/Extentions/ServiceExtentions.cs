9ac4b25edde09f49f77060a629b09b30f39b8371
Muaaz <mmuaaz9@gmail.com>
2022-02-23 13:08:02 +0500

    Added EntityFramework And Identity

--- InstagramBackend/Backend/Extentions/ServiceExtentions.cs
+++ InstagramBackend/Backend/Extentions/ServiceExtentions.cs
@@ -1,7 +1,10 @@
-﻿using LoggerService;
+﻿using Backend.Data;
+using Backend.Models;
+using LoggerService;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
-using System.IO;
 
 namespace Backend.Extentions
 {
@@ -17,6 +20,16 @@ namespace Backend.Extentions
 
         public static void ConfigureLoggerService(this IServiceCollection services) =>
          services.AddScoped<ILoggerManager, LoggerManager>();
+
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+                 services.AddDbContext<AppDbContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentityCore<AppUser>().AddEntityFrameworkStores<AppDbContext>();
+        }
 
     }
 }
